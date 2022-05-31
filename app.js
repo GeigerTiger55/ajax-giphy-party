d4da1466f71a13b562e216bb85bb37d012c856bf
GeigerTiger55 <elise.geiger@gmail.com>
2022-05-31 16:54:08 -0700

    completed part one of assignment

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+"use strict";
+
 console.log("Let's get this party started!");
+
+//Getting the form and adding event handler
+const $giphyForm = $('form');
+$giphyForm.on("submit", handleSubmit);
+
+/**Handle when the form is submitted */
+async function handleSubmit(evt){
+    evt.preventDefault();
+    console.log('in handleSubmit');
+
+    const inputValue = $("input[name='search-term']").val();
+    console.log('in handleSubmit, value is: ', inputValue);
+
+    const response = await axios.get("https://api.giphy.com/v1/gifs/search", {params: {q: inputValue, api_key: 'sdxbpcBL55t0GtSa2YFU6lhmsxTei3LF', limit: 1}});
+    const url = response.data.data[0].url;
+    console.log('response: ', response.data.data[0]);
+
+    createGiphy(url);
+
+}
+
+function createGiphy(url){
+
+}
+
+
+//http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
