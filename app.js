e639e5a29d41dcd3c043ab5acaac944cbddc24bb
Joel <jcbluemel@yahoo.com>
2022-05-31 17:11:45 -0700

    completed part 2 of assignment

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 "use strict";
 
-console.log("Let's get this party started!");
-
 //Getting the form and adding event handler
 const $giphyForm = $('form');
 $giphyForm.on("submit", handleSubmit);
 
-/**Handle when the form is submitted */
-async function handleSubmit(evt){
-    evt.preventDefault();
-    console.log('in handleSubmit');
+/** API call to Giphy, pass 1st url  option to createGiphy func */
+async function handleSubmit(evt) {
+  evt.preventDefault();
+  // console.log('in handleSubmit');
 
-    const inputValue = $("input[name='search-term']").val();
-    console.log('in handleSubmit, value is: ', inputValue);
+  const inputValue = $("input[name='search-term']").val();
+  // console.log('in handleSubmit, value is: ', inputValue);
 
-    const response = await axios.get("https://api.giphy.com/v1/gifs/search", {params: {q: inputValue, api_key: 'sdxbpcBL55t0GtSa2YFU6lhmsxTei3LF', limit: 1}});
-    const url = response.data.data[0].url;
-    console.log('response: ', response.data.data[0]);
-
-    createGiphy(url);
-
-}
-
-function createGiphy(url){
+  const response = await axios.get("https://api.giphy.com/v1/gifs/search", { params: { q: inputValue, api_key: 'sdxbpcBL55t0GtSa2YFU6lhmsxTei3LF', limit: 1 } });
+  const url = response.data.data[0].images.original.url;
+  // console.log(response.data.data[0]);
+  // console.log('response: ', url);
+  createGiphy(url);
 
 }
 
-
-//http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+/** Append gif to gallery using url from api query */
+function createGiphy(giphyUrl) {
+  const $gallery = $('#gallery');
+  const $newGif = $("<div>");
+  const $gif = $("<img>");
+  $gif.attr("src", giphyUrl);
+  $newGif.append($gif);
+  $gallery.append($newGif);
+}
